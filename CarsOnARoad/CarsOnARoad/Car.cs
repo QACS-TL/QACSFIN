f795468cb3dfee965413b86ea48fa408992e3355
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-05-20 15:36:00 +0100

    Add files via upload

--- CarsOnARoad/CarsOnARoad/Car.cs
+++ CarsOnARoad/CarsOnARoad/Car.cs
@@ -2,18 +2,43 @@
 {
     public class Car
     {
-        public string Make { get; set; } 
+        public string Make { get; } 
         public string Model { get; set; }
         private int wheelCount = 4;
-        public string Colour;
-        public int speed = 0;
-        public int topSpeed = 120;
+        public string Colour { get; set; }
+        private int speed;
 
-        public Car(string make="Generic Car", string model="Model X", string colour="Black")
+        public int Speed
+        {
+            get { return speed; }
+            set { 
+                speed = value > topSpeed ? topSpeed : value;
+                speed = value < -10 ? -10 : value; 
+            }
+        }
+
+        private int topSpeed;
+
+        public int TopSpeed
+        {
+            get { return topSpeed; }
+            set { 
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                topSpeed = value; 
+            }
+        }
+
+
+        public Car(string make="Generic Car", string model="Model X", string colour="Black", int topSpeed = 120, int speed = 50)
         {
             this.Make = make;
             this.Model = model;
             this.Colour = colour;
+            this.TopSpeed = topSpeed;
+            this.Speed = speed;
         }
 
         //public Car()//:this("Generic Car", "Model X", "Black")
@@ -55,6 +80,11 @@
         public string Honk()
         {
             return "Beep beep!";
+        }
+        
+        public override string ToString()
+        {
+            return $"{Colour} {Make} {Model} | Top Speed: {TopSpeed} | Current Speed: {Speed}";
         }
 
 
---
+++ CarsOnARoad/CarsOnARoad/Road.cs
@@ -0,0 +1,44 @@
+﻿using CarsOnARoad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsOnARoad
+{
+    public class Road
+    {
+        // Dictionary storing cars
+        public Dictionary<string, Car> cars = new Dictionary<string, Car>
+        {
+            { "AB12CDE", new Car("Ford", "Focus", "Blue", 180, 70) },
+            { "XY34ZRT", new Car("BMW", "320i", "Green", 200, 90 ) },
+            { "LM56OPQ", new Car("Toyota", "Corolla", "Silver", 160, 60) },
+            { "GH78JKL", new Car("Audi", "A4", "Black", 220, 100 ) }
+        };
+
+        // Road order
+        public List<string> carsOnRoad = new List<string>
+        {
+            "AB12CDE", "XY34ZRT", "LM56OPQ", "GH78JKL"
+        };
+
+        public string AttemptOvertake(string plate, int index, string frontPlate, Car car, Car frontCar)
+        {
+            if (car.TopSpeed > frontCar.Speed)
+            {
+                // Swap
+                carsOnRoad[index - 1] = plate;
+                carsOnRoad[index] = frontPlate;
+
+                return $"{plate} overtook {frontPlate}";
+            }
+            else
+            {
+                return "Overtake failed (insufficient speed)";
+            }
+        }
+
+    }
+}

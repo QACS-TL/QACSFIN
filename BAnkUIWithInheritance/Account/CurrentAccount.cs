15ade1dd55c77ff4278b3f53c956b8a1558e4d5e
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-05-21 15:02:15 +0100

    Add files via upload

---
+++ BAnkUIWithInheritance/Account/CurrentAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class CurrentAccount: Account
+    {
+        public  decimal OverDraftLimit { get; set; }
+
+        public CurrentAccount(string name, string accountNumber, decimal balance, decimal overdraftLimit): base(name, accountNumber) 
+        {
+            this.OverDraftLimit = overdraftLimit;
+        }
+
+        public override decimal Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value < -OverDraftLimit)
+                {
+                    value = OverDraftLimit;
+                }
+                balance = value;
+            }
+        }
+
+        public override decimal Debit(decimal amount)
+        {
+            if ((Balance - amount) > -OverDraftLimit)
+                Balance -= amount;
+            return Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().FullName} for {Name} Account Number: {AccountNumber}'s balance is {Balance:c}. Overdraft Limit is {OverDraftLimit:c}";
+        }
+    }
+}
---
+++ BAnkUIWithInheritance/Account/SavingsAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class SavingsAccount: Account
+    {
+        public decimal InterestRate { get; set; }
+
+        public SavingsAccount(string name, string accountNumber, decimal balance, decimal interestRate) : base(name, accountNumber, balance)
+        {
+            this.InterestRate = interestRate;
+        }
+
+        public override decimal Debit(decimal amount)
+        {
+            if (Balance - amount < 0)
+                Balance -= amount;
+            return Balance;
+        }
+
+
+        public decimal CalcInterest()
+        {
+            Balance += Balance * InterestRate;
+            return Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().FullName} for {Name} Account Number: {AccountNumber}'s balance is {Balance:c}. Interest rate is {InterestRate:c}";
+        }
+    }
+}
